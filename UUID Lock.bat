e105eae86f151b89152d084d27407bdf0135e740
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2023-12-19 07:33:40 +0000

    Update UUID Lock.bat

--- UUID Lock.bat
+++ UUID Lock.bat
@@ -1,23 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "Whitelisted=00000000-0000-0000-0000-00D8613A05B4"
-set "Blacklisted=00000000-0000-0000-0000-000000000000"
+set "Whitelisted=00929036-5104-11ED-B377-34824127DF45"
 
 :: wmic csproduct get uuid
-for /f "tokens=2 delims==" %%A in ('wmic csproduct get uuid /value ^| find "="') do set "serialnumber=%%A"
+for /f "tokens=2 delims==" %%A in ('wmic csproduct get uuid /value ^| find "="') do set "UUID=%%A"
 
-if "%serialnumber%"=="!Whitelisted!" goto:authorized do (
-	if "%serialnumber%"=="!Blacklisted!" goto:unauthorized
+if "%UUID%"=="!Whitelisted!" (
+    goto :authorized
+) else (
+    goto :unauthorized
 )
-exit /b 0
 
 :authorized
-echo Whitelisted&pause
-exit /b 0
+echo Whitelisted & pause
+goto :EOF
 
 :unauthorized
-echo Blacklisted&pause
-exit /b 0
+echo Blacklisted & pause
+goto :EOF
 
-FF FE 0A 0D
+exit /b 0
